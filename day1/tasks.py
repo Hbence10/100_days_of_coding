1f4a204ce241a4f420c284d25e8aa46fc56a62e9
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-03-11 23:25:41 +0100

    day 1

---
+++ day1/tasks.py
@@ -0,0 +1,21 @@
+# Task 1
+print("Hello world!")
+
+# Task 2
+print("Hello world!\nHello world!\nHello world!")
+print("Hello Angela")
+
+# Task 3
+print("Your name: ")
+name = str(input())
+
+# Task 4
+print("My name is" + " " + "Angela")
+
+# Task 5
+print(len(input("What is your name?")))
+
+# Task 6
+name = "Angela"
+length = len(name)
+print(length)
