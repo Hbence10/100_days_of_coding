925b754fb967a0a9faa5cc6a45a008d4878c04cd
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-03-20 11:44:09 +0100

    day 2

---
+++ day2/main.py
@@ -0,0 +1,15 @@
+import math
+
+print("Welcome to the tip calculator!")
+totalBill = float(input("What was the total bill? $"))
+tipPercentage = int(input("How much tip would you like to give? 10, 12, or 15? "))
+
+possibleTips = [10, 12, 15]
+while tipPercentage not in possibleTips:
+    tipPercentage = int(input("How much tip would you like to give? 10, 12, or 15? "))
+
+tipAmount = totalBill / 100 * tipPercentage
+peopleAmount = int(input("How many people to split the bill? "))
+
+result = round((totalBill + tipAmount) / peopleAmount, 2)
+print(f"Each person should pay: {(totalBill + tipAmount) / peopleAmount}")
