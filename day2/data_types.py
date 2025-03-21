aa98c6aa57e2197e891f57dfbe79e8e0cdb97953
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-03-21 17:26:40 +0100

    start day 3

---
+++ day2/data_types.py
@@ -0,0 +1,15 @@
+#String
+print("123" + "345")
+
+#Integer = Whole numbers
+print(123 + 345)
+
+#Large numbers
+print(123_456_789)
+
+#Float = floating point numbers
+print(3.143553)
+
+#Booleans
+print(False)
+print(True)
